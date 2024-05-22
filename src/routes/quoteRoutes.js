cdc9cd49c06fb5da7c7e566dfb88c910e69fe29a
juweyriyo <125019462+juweyriyo@users.noreply.github.com>
2024-05-22 15:20:46 +0300

    Add PUT route to quoteRoutes.js for updating quotes

--- src/routes/quoteRoutes.js
+++ src/routes/quoteRoutes.js
@@ -3,5 +3,6 @@ import { registerQuote } from "../controllers/Quote.js";
 const quote = express.Router();
 
 quote.post("/register", registerQuote)
+quote.put("/update/:id", registerQuote)
 
 export default quote
